437942af29a12a13fac67c22fc51af9da4eeeccf
Toa W Carney <twcarney47@xtra.co.nz>
2018-06-16 22:20:03 +1200

    Code commenting

--- script.js
+++ script.js
@@ -7,38 +7,37 @@ const inputs = ['firstName', 'lastName', 'age']; // IDs of all form inputs
 3 = over boundary
 */
 
-var firstNameElement = document.getElementById(inputs[0])
+var firstNameElement = document.getElementById(inputs[0]) //Get all the form elements and store them in variables
 var lastNameElement = document.getElementById(inputs[1])
 var ageElement = document.getElementById(inputs[2])
 
-var firstNames = [];
+var firstNames = []; //Create empty arrays
 var lastNames = [];
 var ages = [];
 
-function getValues() {
-  firstName = firstNameElement.value;
+function getValues() { //Function for getting the values from the form (and also checking and storing them)
+  firstName = firstNameElement.value; // Get the values and store them
   lastName = lastNameElement.value;
   age = ageElement.value;
 
-  switch (verify(firstName, 'name')) {
-    case 0:
+  switch (verify(firstName, 'name')) { // Verify that firstName is a valid name
+    case 0: // Is valid
       firstNameElement.style.borderColor = "initial"
       break;
-    case 1:
+    case 1: // Is invalid (name is empty)
       firstNameElement.style.borderColor = "red";
       break;
-    case 2:
-      firstNameElement.style.borderColor = "red";
-    case 3:
+    case 3: // Is invalid (name is too long)
       firstNameElement.style.borderColor = "red";
       break;
 
   }
-  verify(lastName, 'name');
+  
+  verify(lastName, 'name'); //TODO: put these in switch statements
   verify(age, 'age');
 
-  firstNames.push(firstName);
-  lastNames.push(lastName);
+  firstNames.push(firstName); // Push the values to the array
+  lastNames.push(lastName);   // TODO: Push the values only when they have been verified (when they are valid)
   ages.push(age);
 }
 
@@ -55,6 +54,7 @@ function verify(subject, type) {
         return(0); //return 0 if okay
       }
       break;
+    
     case 'age':
       if (subject < 16) {
         return(2); //error code 2: Under boundary (age too low)
