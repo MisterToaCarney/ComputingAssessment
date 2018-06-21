06ef05b0b45df9e3b9d03694ea3154b879baae32
Toa Carney <twcarney47@xtra.co.nz>
2018-06-21 16:29:39 +1200

    Cost calculations and output table

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const inputs = ['firstName', 'lastName', 'age']; // IDs of all form inputs
-const errors = ['firstNameError', 'lastNameError', 'ageError'];
+//script.js written by Toa Carney
+
 /* List of error codes:
 0 = no error
 1 = no content
 2 = under boundary
 3 = over boundary
 */
+
+/*
+List of costs:
+1 person: $500 / Person
+2 people: $450 / Person
+3-4 people: $430 / Person
+4-8 people: $400 / Person
+9+ people: $390 / Person
+*/
+
+const inputs = ['firstName', 'lastName', 'age']; // IDs of all form inputs
+const errors = ['firstNameError', 'lastNameError', 'ageError'];
+
+const maxPeople = 20; //Define a maximum of 20 people.
+
 
 var firstNameElement = document.getElementById(inputs[0]) //Get all the form elements and store them in variables
 var lastNameElement = document.getElementById(inputs[1])
@@ -18,6 +33,7 @@ var ageError = document.getElementById(errors[2]);
 var firstNames = []; //Create empty arrays
 var lastNames = [];
 var ages = [];
+var hasHealthIssues = [];
 
 function getValues() { //Function for getting the values from the form (and also checking and storing them)
   firstName = firstNameElement.value; // Get the values and store them
@@ -89,7 +105,7 @@ function getValues() { //Function for getting the values from the form (and also
   }
   if (isValid == true) {
     firstNames.push(firstName); // Push the values to the array
-    lastNames.push(lastName); 
+    lastNames.push(lastName);
     ages.push(age);
   }
 }
@@ -100,7 +116,7 @@ function verify(subject, type) {
       if (subject.length == 0) {
         return(1); //error code 1: No content
       }
-      else if(subject.length > 150) {
+      else if(subject.length > 1000) {
         return(3); //error code 3: Over boundary
       }
       else {
@@ -125,6 +141,52 @@ function verify(subject, type) {
     default:
       console.error("Fatal: Data type '" + type + "' does not exist.");
       return(1);
+  }
+}
+
+function calculateCosts(ages, healths) { // Takes list of ages and health issues and calulates cost
+  if (ages.length != healths.length) {
+    console.error("Fatal: Arrays do not match");
+    return("ERROR");
+  }
+  var length = ages.length;
+
+  var cost = 0;
+  switch (length) {
+    case 0:
+      cost = 0;
+    case 1: //If one person
+      cost = 500 * length;
+      break;
+    case 2:
+      cost = 450 * length;
+      break;
+    case 3:
+    case 4:
+      cost = 430 * length;
+      break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      cost = 400 * length;
+      break;
+    default:
+      cost = 390 * length;
+  }
+}
+
+function drawTable() {
+  /* Table layout
+  Cell 0: number
+  Cell 1: name
+  Cell 2: Ages
+  Cell 3: Has health issues
+  */
+  var root = document.getElementById("outputTable");
+  var
+  for (var i = 0; i < root.rows.length; i++) {
+    root.rows[i]
   }
 }
 
